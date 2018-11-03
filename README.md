471e716138a1441edef990e7581e4f7047160b29
ahmedreda89 <e.ahmedreda@gmail.com>
2018-11-04 01:01:38 +0200

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# arcade-game
+# Cross to the river
+
+A really nice arcade game!, go on try it out!!
+
+## Getting Started
+
+Start by downloading the game ZIP file to your computer, then Unzip it to a folder in your desired destination, after that open the game folder and double click on the index file, it will open up the game in your default browser.
+
+### Prerequisites
+
+Nothing to install, just make sure that Javascript is allowed on your browser, if the game didn't start or looked a little corrupted.
+
+### How To Play
+
+* Using the keyboard arrows, try to avoid the bugs and reach to the water side.
+
+## Authors
+
+* **Ahmed Reda** - [Click here to view some work](https://github.com/AhmedReda89)
+
+* See also the list of [contributors](https://github.com/AhmedReda89/Arcade-Game/graphs/contributors) who participated in this project.
+
+## License
+
+You are free to play this game as much as you want, However I didn't make for commercial use, or to be included in other websites.
+
+## Acknowledgments
+
+* Thanks alot Udacity Team for the Nano degree, the game styles and JS starter functionality, still loving you guys.
+
--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.initialLocationx = Math.random()*400;
-    this.initialLocationy = Math.floor(Math.random() * (220 - 80) ) + 50;
+    this.initialLocationx = Math.random() * 400;
+    this.initialLocationy = Math.floor(Math.random() * (220 - 80)) + 50;
     this.speed = 1 + Math.random();
     this.x = this.initialLocationx;
     this.y = this.initialLocationy;
@@ -17,75 +17,71 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 
     // To move the enemies 
-    this.x += dt+1*this.speed;
+    this.x += dt + 1 * this.speed;
 
     this.checkCollisions();
 };
 
-Enemy.prototype.checkCollisions = function(){
+Enemy.prototype.checkCollisions = function () {
     // To check for collision 
     if (player.x < this.x + this.width &&
         player.x + player.width > this.x &&
         player.y < this.y + this.height &&
-        player.y + player.height > this.y){
-            gameOver();
-        };
+        player.y + player.height > this.y) {
+        gameOver();
+    };
 }
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = function(){
+var player = function () {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
     this.width = 70;
     this.height = 70;
-    this.handleInput = function(key){
+    this.handleInput = function (key) {
         switch (key) {
             case 'left':
-              if(this.x > 10)
-              this.x -= 50;
-              break;
+                if (this.x > 10)
+                    this.x -= 50;
+                break;
 
             case 'up':
-              if(this.y > 0){
-                this.y -= 50;
-              }
-              else{
-                  winGameOver();
-              }
-              break;
-              
+                if (this.y > 0) {
+                    this.y -= 50;
+                }
+                else {
+                    winGameOver();
+                }
+                break;
+
             case 'right':
-              if(this.x < 400)
-              this.x += 50;
-              break;
+                if (this.x < 400)
+                    this.x += 50;
+                break;
 
             case 'down':
-              if(this.y < 450)
-              this.y += 50;
-              break;
-          }
+                if (this.y < 450)
+                    this.y += 50;
+                break;
+        }
     };
 };
 
-player.prototype.update = function(dt){
-
-};
-
-player.prototype.render = function() {
+player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -96,15 +92,15 @@ var player = new player();
 // Place the player object in a variable called player
 var allEnemies = [];
 var enemy1 = new Enemy();
-    var enemy2 = new Enemy();
-    var enemy3 = new Enemy();
-    var enemy4 = new Enemy();
-    allEnemies.push(enemy1);
-    allEnemies.push(enemy2);
-    allEnemies.push(enemy3);
-    allEnemies.push(enemy4);
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
 
-setInterval(function(){
+setInterval(function () {
 
     var enemy1 = new Enemy();
     var enemy2 = new Enemy();
@@ -112,29 +108,29 @@ setInterval(function(){
     allEnemies.push(enemy1);
     allEnemies.push(enemy2);
     allEnemies.push(enemy3);
-    enemy1.x = enemy2.x = enemy3.x = Math.random()*(-150);
+    enemy1.x = enemy2.x = enemy3.x = Math.random() * (-150);
 
-},4000)
+}, 4000)
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-    if(modal && !modal.opened){
+    if (modal && !modal.opened) {
         player.handleInput(allowedKeys[e.keyCode]);
     }
 });
 
-function gameOver(){
+function gameOver() {
     //alert('Game Over');
     openModal("lose");
 }
-function winGameOver(){
+function winGameOver() {
     //alert('You Win!');
     openModal("win");
 }
@@ -142,39 +138,39 @@ function winGameOver(){
 
 class Modal {
     constructor(overlay) {
-      this.overlay = overlay;
-      this.opened = false;
-      const closeButton = overlay.querySelector('.button-close')
-      closeButton.addEventListener('click', this.close.bind(this));
-      overlay.addEventListener('click', e => {
-        if (e.srcElement.id === this.overlay.id) {
-          this.close();
-        }
-      });
+        this.overlay = overlay;
+        this.opened = false;
+        const closeButton = overlay.querySelector('.button-close')
+        closeButton.addEventListener('click', this.close.bind(this));
+        overlay.addEventListener('click', e => {
+            if (e.srcElement.id === this.overlay.id) {
+                this.close();
+            }
+        });
     }
     open(res) {
-      this.overlay.classList.remove('is-hidden');
-      this.opened = true;
-      var status = document.querySelector('.modal .content h3');
-      if(res == 'win'){
-        status.innerHTML = "You Win!";
-        gameReset();
-      }else if(res == 'lose'){
-        status.innerHTML = "You Lose!";
-        gameReset();
-      }
+        this.overlay.classList.remove('is-hidden');
+        this.opened = true;
+        var status = document.querySelector('.modal .content h3');
+        if (res == 'win') {
+            status.innerHTML = "You Win!";
+            gameReset();
+        } else if (res == 'lose') {
+            status.innerHTML = "You Lose!";
+            gameReset();
+        }
     }
-  
-    close() {
-      this.overlay.classList.add('is-hidden');
-      this.opened = false;
-    }
-  }
-  const modal = new Modal(document.querySelector('.modal-overlay'));
-  window.openModal = modal.open.bind(modal);
-  //window.openModal();
 
-function gameReset(){
+    close() {
+        this.overlay.classList.add('is-hidden');
+        this.opened = false;
+    }
+}
+const modal = new Modal(document.querySelector('.modal-overlay'));
+window.openModal = modal.open.bind(modal);
+//window.openModal();
+
+function gameReset() {
     allEnemies = [];
     player.x = 200;
     player.y = 400;
